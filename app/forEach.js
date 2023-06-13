68c6dc4f0c3c5086d7a71115be80d6daced3932a
Pedro Augusto Barbosa De Souza <81322227+pedroabs1@users.noreply.github.com>
2023-06-13 16:03:35 -0400

    Update forEach.js

--- app/forEach.js
+++ app/forEach.js
@@ -6,21 +6,28 @@ function exibirCardapio(cardapio) {
         // console.log(descricao)
         itensDescricao = descricao.split(/\n/);
         // console.log(itensDescricao)
+        const descrição_texto for (let contador = 0; contador < itensDescricao.length; contador++) {
+
+                // console.log(itensDescricao.length)
+
+                inserirItensCardapio.innerHTML +=`<li class="ingredientes--lanche">`
+
+                inserirItensCardapio.innerHTML += itensDescricao[contador];
+
+                // console.log('descricao[' + contador +']:', itensDescricao[contador])
+
+                inserirItensCardapio.innerHTML +=`</li>`
+
+            }
 
     inserirItensCardapio.innerHTML += `
         <ul class="cartao__lanche">
             <img src="${item.imagem}" alt="${item.alt}" class="imagem--lanche">
             <h3 class="titulo--lanche">${item.titulo}</h3>
-            <ul class="container__descricao--lanche">`
-            for (let contador = 0; contador < itensDescricao.length; contador++) {
-                // console.log(itensDescricao.length)
-                inserirItensCardapio.innerHTML +=`<li class="ingredientes--lanche">`
-                inserirItensCardapio.innerHTML += itensDescricao[contador];
-                // console.log('descricao[' + contador +']:', itensDescricao[contador])
-                inserirItensCardapio.innerHTML +=`</li>`
-            }
-            inserirItensCardapio.innerHTML +=`</ul>
-            <div class="container__comprar">
+            <ul class="container__descricao--lanche">`+ descrição_texto + `</ul>
+
+
+<div class="container__comprar">
             <h2 class="preco--lanche">Apenas R$ ${item.preco}</h2>
             <button name="Comprar" class="comprar--lanche" id="${item.titulo}">Comprar</button>
         </div>
